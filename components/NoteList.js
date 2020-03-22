4409197c0cb141dfa82d708e8714a73dd23a9f14
Sohrab Taee <sohrab.taee@gmail.com>
2020-03-22 21:11:53 +0100

    Added styling for NoteList. Installed react-icons.

--- components/NoteList.js
+++ components/NoteList.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react'
 import { GlobalContext } from '../context/GlobalState'
+import { MdClose, MdEdit } from 'react-icons/md'
 
 const NoteList = () => {
   const { notes } = useContext(GlobalContext)
@@ -7,9 +8,17 @@ const NoteList = () => {
   return (
     <ul>
       {notes.map((note) => (
-        <li key={note.id}>
-          <h3>{note.title}</h3>
-          <div>{note.content}</div>
+        <li key={note.id} className="mt-4 bg-gray-100 p-2 relative">
+          <h3 className="pr-16">{note.title}</h3>
+          <div className="mt-1">{note.content}</div>
+          <div className="absolute top-0 right-0 text-xl">
+            <button className="action-button text-blue-600">
+              <MdEdit />
+            </button>
+            <button className="action-button text-red-600">
+              <MdClose />
+            </button>
+          </div>
         </li>
       ))}
     </ul>
