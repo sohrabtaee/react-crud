864952de66fa5bb182b043a6fc78ed75544ecb87
Sohrab Taee <sohrab.taee@gmail.com>
2020-03-23 21:10:20 +0100

    Set up unit testing.

--- context/GlobalState.js
+++ context/GlobalState.js
@@ -3,7 +3,24 @@ import NotesReducer from './NotesReducer'
 import { REMOVE_NOTE, ADD_NOTE, EDIT_NOTE } from './Constants'
 
 const initialState = {
-  notes: [],
+  notes: [
+    {
+      id: 1,
+      title: 'Title',
+      content:
+        'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod commodi perspiciatis nemo quia. Enim labore blanditiis unde qui necessitatibus repellat asperiores explicabo impedit non fugit! Quidem adipisci explicabo autem maiores!',
+    },
+    {
+      id: 2,
+      title: 'Very very very long title for testing',
+      content: 'Some content',
+    },
+    {
+      id: 3,
+      title: 'Normal Title',
+      content: '',
+    },
+  ],
 }
 
 export const GlobalContext = createContext(initialState)
