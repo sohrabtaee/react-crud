9efeb7a7b9c56f49cd01e693e537064a094f1621
Sohrab Taee <sohrab.taee@gmail.com>
2020-04-15 12:48:03 +0200

    Set up unit tests.

--- components/AddNoteButton.js
+++ components/AddNoteButton.js
@@ -3,7 +3,10 @@ import Link from 'next/link'
 
 const AddNoteButton = () => (
   <Link href="/add">
-    <a className="button border-0 bg-green-600 text-white text-xl">
+    <a
+      className="button border-0 bg-green-600 text-white text-xl"
+      data-testid="button"
+    >
       Add a note
     </a>
   </Link>
---
+++ tests/unit/components/AddNoteButton.spec.js
@@ -0,0 +1,17 @@
+describe('Add Note Button', () => {
+  it('should render the button', () => {})
+})
+
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import AddNoteButton from '../../../components/AddNoteButton'
+
+describe('Add Note Button', () => {
+  it('should render the button', () => {
+    render(<AddNoteButton />)
+    const element = screen.getByTestId('button')
+
+    expect(element).toHaveTextContent('Add a note')
+  })
+})
