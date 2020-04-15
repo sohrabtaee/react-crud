768ede7839883dfefcb67474b4c8d637ca065177
Sohrab Taee <sohrab.taee@gmail.com>
2020-04-15 14:29:30 +0200

    Added tilde alias to Jest

--- tests/unit/components/AddNoteButton.spec.js
+++ tests/unit/components/AddNoteButton.spec.js
@@ -1,11 +1,7 @@
-describe('Add Note Button', () => {
-  it('should render the button', () => {})
-})
-
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
-import AddNoteButton from '../../../components/AddNoteButton'
+import AddNoteButton from '~/components/AddNoteButton'
 
 describe('Add Note Button', () => {
   it('should render the button', () => {
