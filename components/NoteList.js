48af6638e94b90691fa03c8eed45a8b5aaf30ff9
Sohrab Taee <sohrab.taee@gmail.com>
2020-04-17 13:35:29 +0200

    Added some unit tests for NoteList component

--- components/NoteList.js
+++ components/NoteList.js
@@ -5,24 +5,30 @@ import { MdClose, MdEdit } from 'react-icons/md'
 
 const NoteList = ({ notes, onDelete }) => {
   return (
-    <ul className="flex flex-wrap">
+    <ul className="flex flex-wrap" data-testid="notes">
       {notes.map((note) => (
-        <li key={note.id} className="note-card">
+        <li key={note.id} className="note-card" data-testid="note">
           <header className="note-card-header">
             <h2 className="text-lg w-full">
               <Link href="/view/[id]" as={`/view/${note.id}`}>
-                <a className="block truncate">{note.title}</a>
+                <a className="block truncate" data-testid="view-link">
+                  {note.title}
+                </a>
               </Link>
             </h2>
             <div className="absolute top-0 right-0 z-10">
               <Link href="/edit/[id]" as={`/edit/${note.id}`}>
-                <a className="action-button text-blue-600">
+                <a
+                  className="action-button text-blue-600"
+                  data-testid="edit-link"
+                >
                   <MdEdit />
                 </a>
               </Link>
               <button
                 className="action-button text-red-600 ml-1"
                 onClick={() => onDelete(note.id)}
+                data-testid="delete-button"
               >
                 <MdClose />
               </button>
---
+++ tests/unit/components/NotesList.spec.js
@@ -0,0 +1,73 @@
+import React from 'react'
+import { render, screen, fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import NoteList from '~/components/NoteList'
+
+describe('Notes List', () => {
+  describe('No notes', () => {
+    it('should show nothing', () => {
+      render(<NoteList notes={[]} />)
+      const notes = screen.getByTestId('notes')
+
+      expect(notes.childNodes.length).toEqual(0)
+    })
+  })
+
+  describe('With notes', () => {
+    const onDelete = jest.fn()
+    const notes = [
+      {
+        id: 1,
+        title: 'title',
+        content: 'content',
+      },
+      {
+        id: 2,
+        title: 'title2',
+        content: 'content2',
+      },
+    ]
+    beforeEach(() => {
+      render(<NoteList notes={notes} onDelete={onDelete} />)
+    })
+
+    it('should show a list of notes', () => {
+      const notes = screen.getByTestId('notes')
+
+      expect(notes.childNodes.length).toEqual(2)
+    })
+
+    it('should show a proper title for each note', () => {
+      const noteList = screen.getAllByTestId('note')
+
+      expect(noteList[0]).toHaveTextContent('title')
+      expect(noteList[1]).toHaveTextContent('title2')
+    })
+
+    it('should show a delete button for each note', () => {
+      const deleteButtonList = screen.getAllByTestId('delete-button')
+
+      expect(deleteButtonList.length).toEqual(2)
+    })
+
+    it('should emit a delete event with note id when user click on delete button', () => {
+      const deleteButton = screen.getAllByTestId('delete-button')[0]
+      fireEvent.click(deleteButton)
+
+      expect(onDelete).toHaveBeenCalledTimes(1)
+      expect(onDelete).toHaveBeenCalledWith(notes[0].id)
+    })
+
+    it('should go to view page when user clicks on a note', () => {
+      const viewLink = screen.getAllByTestId('view-link')[0]
+
+      expect(viewLink.getAttribute('href')).toEqual(`/view/${notes[0].id}`)
+    })
+
+    it('should go to edit page when user clicks on the edit button', () => {
+      const editLink = screen.getAllByTestId('edit-link')[0]
+
+      expect(editLink.getAttribute('href')).toEqual(`/edit/${notes[0].id}`)
+    })
+  })
+})
