13d69174d13d7ceab110092613ca08562e25fcaf
Sohrab Taee <sohrab.taee@gmail.com>
2020-04-22 12:10:13 +0200

    Added PWA functionality

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 const path = require('path')
 const webpack = require('webpack')
+const withPWA = require('next-pwa')
 
-module.exports = {
+module.exports = withPWA({
   webpack: (config) => {
     config.resolve.alias['~'] = path.resolve(__dirname)
     return config
   },
-}
+  pwa: {
+    dest: 'public',
+  },
+})
